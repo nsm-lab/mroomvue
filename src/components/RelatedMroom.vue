23334ff85717a6a18eaa807e1bf33709dd7d685e
nsm-lab <nsm180818@gmail.com>
2020-03-04 13:43:19 +0700

    upload project vue

---
+++ src/components/RelatedMroom.vue
@@ -0,0 +1,121 @@
+<template>
+    <!-- Related Products Section End -->
+    <div class="related-products spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section-title">
+                        <h2>Related Products</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3 col-sm-6">
+                    <div class="product-item">
+                        <div class="pi-pic">
+                            <img src="img/products/women-1.jpg" alt="" />
+                            <ul>
+                                <li class="w-icon active">
+                                    <a href="#"><i class="icon_bag_alt"></i></a>
+                                </li>
+                                <li class="quick-view">
+                                    <router-link to="/product">+ Quick view</router-link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="pi-text">
+                            <div class="catagory-name">Coat</div>
+                            <router-link to="/product">
+                                <h5>Pure Pineapple</h5>
+                            </router-link>
+                            <div class="product-price">
+                                $14.00
+                                <span>$35.00</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="product-item">
+                        <div class="pi-pic">
+                            <img src="img/products/women-2.jpg" alt="" />
+                            <ul>
+                                <li class="w-icon active">
+                                    <a href="#"><i class="icon_bag_alt"></i></a>
+                                </li>
+                                <li class="quick-view">
+                                    <router-link to="/product">+ Quick View</router-link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="pi-text">
+                            <div class="catagory-name">Shoes</div>
+                            <router-link to="/product">
+                                <h5>Guangzhou sweater</h5>
+                            </router-link>
+                            <div class="product-price">
+                                $13.00
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="product-item">
+                        <div class="pi-pic">
+                            <img src="img/products/women-3.jpg" alt="" />
+                            <ul>
+                                <li class="w-icon active">
+                                    <a href="#"><i class="icon_bag_alt"></i></a>
+                                </li>
+                                <li class="quick-view">
+                                    <router-link to="/product">+ Quick View</router-link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="pi-text">
+                            <div class="catagory-name">Towel</div>
+                            <router-link to="/product">
+                                <h5>Pure Pineapple</h5>
+                            </router-link>
+                            <div class="product-price">
+                                $34.00
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6">
+                    <div class="product-item">
+                        <div class="pi-pic">
+                            <img src="img/products/women-4.jpg" alt="" />
+                            <ul>
+                                <li class="w-icon active">
+                                    <a href="#"><i class="icon_bag_alt"></i></a>
+                                </li>
+                                <li class="quick-view">
+                                    <router-link to="/product">+ Quick View</router-link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="pi-text">
+                            <div class="catagory-name">Towel</div>
+                            <router-link to="/product">
+                                <h5>Converse Shoes</h5>
+                            </router-link>
+                            <div class="product-price">
+                                $34.00
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Related Products Section End -->
+</template>
+
+<script>
+
+export default {
+  name: 'RelatedMroom'
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+
+    <HeaderMroom />
+    <HeroMroom />
+    <SliderMroom />
+    <InstaMroom />
+    <PartnerMroom />
+    <FooterMroom />
+
+  </div>
+</template>
+
+<script>
+// // @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderMroom from "@/components/HeaderMroom.vue";
+import HeroMroom from "@/components/HeroMroom.vue";
+import SliderMroom from "@/components/SliderMroom.vue";
+import InstaMroom from "@/components/InstaMroom.vue";
+import PartnerMroom from "@/components/PartnerMroom.vue";
+import FooterMroom from "@/components/FooterMroom.vue";
+
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderMroom,
+    HeroMroom,
+    SliderMroom,
+    InstaMroom,
+    PartnerMroom,
+    FooterMroom
+  }
+}
+</script>
